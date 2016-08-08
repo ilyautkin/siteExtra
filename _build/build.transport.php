7f42c3e45f94406a3a8d4ab807b45db8c8d1941e
Ilya Utkin <ilyautkin@mail.ru>
2016-08-08 17:44:07 +0300

    1.2.0-beta

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,5 +14,5 @@ $resolvers = array(
     'fix_fastuploadtv',
     'manager_customisation'
 );
-$builder = new siteBuilder('site', '1.1.9', 'beta', $resolvers);
+$builder = new siteBuilder('site', '1.2.0', 'beta', $resolvers);
 $builder->build();
